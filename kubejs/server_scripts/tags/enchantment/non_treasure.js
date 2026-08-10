28a74d9d20428874d154ab05695170fee95d7f4f
BrunoGoldbergFerro <86776985+BrunoGoldbergFerro@users.noreply.github.com>
2026-08-10 19:35:35 -0300

    Update non_treasure.js
    read previous

--- kubejs/server_scripts/tags/enchantment/non_treasure.js
+++ kubejs/server_scripts/tags/enchantment/non_treasure.js
@@ -1,5 +1,4 @@
 ServerEvents.tags('enchantment', event => {
       event.add('minecraft:non_treasure', 'buildandthrive:melee_protection', 'buildandthrive:magic_protection')
-      event.remove('minecraft:non_treasure', 'minecraft:silk_touch')
 
 })

259a079bc65db7f4e46951c3f1ffc4258b3edd0c
BrunoGoldbergFerro <86776985+BrunoGoldbergFerro@users.noreply.github.com>
2026-08-10 19:29:45 -0300

    Delete kubejs/server_scripts/loot/trial_reach.js
    removing reach

--- kubejs/server_scripts/loot/trial_reach.js
+++
@@ -1,6 +0,0 @@
-LootJS.lootTables(event => {
-    event
-    .getLootTable("minecraft:chests/trial_chambers/reward_unique")
-    .firstPool()
-    .addEntry(LootEntry.reference("buildandthrive:chests/enchantment_reach"))
-})
